6278e5242064a5ad46a508cf63d4a0d60f4971fd
faizal <syedfaizal905@gmail.com>
2024-07-22 22:45:56 +0530

    Generic

--- src/class_and_object/Generics/InAndOut.kt
+++ src/class_and_object/Generics/InAndOut.kt
@@ -1,31 +1,33 @@
 package class_and_object.Generics
 
 
-/**
- *
- *
- * 
- */
-
-
 fun main(){
+    /**
+     * by using generic it's return what you give, by default in other concepts subclass is also superclass. But Not in Generic without using In and Out keys
+     *
+     * out - when your reference type may be it's subClass type, in this case we can use out keyword.
+     * in - when your reference type may be its superclass type, in this case we can use in keyword.
+     * otherwise its only accept its own type reference.
+     */
+
+
 
     var fixedGenericType: FixedGenericType<FastFood> =
-        FixedGenericType<FastFood>() //✅
-    //FixedGenericType<Food>() //❌
-    //FixedGenericType<Burger>() //❌
+        FixedGenericType<FastFood>() //✅ FastFood is needed. so its accept it.
+        //FixedGenericType<Food>() //❌ Food is superclass of FastFood, but its not accept in generic concept
+        //FixedGenericType<Burger>() //❌ Burger is subclass of FastFood, But it's not accept in generic concept
 
 
     var consumerGenericType: ConsumerGenericType<FastFood> =
-        ConsumerGenericType<FastFood>() //✅
-    ConsumerGenericType<Food>() //✅
-    ConsumerGenericType<Burger>() //❌
+        ConsumerGenericType<FastFood>() //✅ FastFood is needed. so its accepted.
+    ConsumerGenericType<Food>() //✅ Food is superClass of FastFood, here we are using in keyword, so its accepted.
+    ConsumerGenericType<Burger>() //❌ Burger is subClass of FastFood, but its not accept in generic concept.
 
 
     var producerGenericType: ProducerGenericType<FastFood> =
         ProducerGenericType<FastFood>() //✅
     ProducerGenericType<Food>() //❌
-    ProducerGenericType<Burger>() //✅
+    ProducerGenericType<Burger>() //✅ Burger is subClass of FastFood, here we are using out keyword, so its accepted.
 
 }
 
